15484cc92fb1ea1cb4391c0f0d1f30e35901995d
amiralirashidi <amiralii.rashidii@gmail.com>
2024-02-14 13:29:03 +0330

    update cv

--- src/app/work-sample/work-sample.component.ts
+++ src/app/work-sample/work-sample.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-work-sample',
   templateUrl: './work-sample.component.html',
-  styleUrls: ['./work-sample.component.scss']
+  styleUrls: ['./work-sample.component.scss'],
 })
 export class WorkSampleComponent {
+  canShowModal: boolean = false;
+  modalImage: string | undefined;
 
+  changeImage(image?: string) {
+    if (image) {
+      this.canShowModal = true;
+      this.modalImage = image;
+    } else {
+      this.canShowModal = false;
+      this.modalImage = undefined;
+    }
+  }
 }
